7859aa176ab0f85bbd6b91df5f90ccbc3ef81daf
Shehara_r <ranathisarashehara01@gmail.com>
2022-06-03 13:34:22 +0530

    update api

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://wismar.stepzen.net/api/vetoed-rabbit/__graphql',
+  uri: 'https://wismar.stepzen.net/api/piquant-leopard/__graphql',
   headers: {
     Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
   },
